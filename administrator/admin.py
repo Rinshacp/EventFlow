24f9f1cd5536854396c8dd6f91147f57e9bb6c63
victus hp <rinshabasheercp@gmail.com>
2024-12-13 15:39:53 +0530

    first commit

---
+++ administrator/admin.py
@@ -0,0 +1,32 @@
+
+from django.contrib import admin
+
+from administrator.models import *
+
+
+# Register your models here.
+
+admin.site.register(LoginTable)
+admin.site.register(clubtable)
+admin.site.register(schooltable)
+admin.site.register(collegetable)
+admin.site.register(User)
+admin.site.register(activities)
+admin.site.register(complaints)
+admin.site.register(notification)
+admin.site.register(servicetable)
+admin.site.register(feedback)
+
+
+
+
+# Register your models here.
+
+admin.site.register(event)
+
+
+
+
+
+
+
